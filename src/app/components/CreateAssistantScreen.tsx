82507231ae0ead5de7d39947bb45caadc16656c4
Leonardo Rocha <leonardo.rocha@grupoitss.com.br>
2026-05-25 16:49:18 -0300

    fix: corrige pb-80 removendo h-full/size-full do chain de wrappers do form [claude]
    Containers internos passam de size-full/h-full para w-full (altura pelo conteudo).
    Isso permite que pb-[80px] apareça como respiro real apos o ultimo campo.

--- src/app/components/CreateAssistantScreen.tsx
+++ src/app/components/CreateAssistantScreen.tsx
@@ -113,9 +113,9 @@ export default function CreateAssistantScreen({ onBack }: CreateAssistantScreenP
 
         {/* Form Content */}
         <div className="bg-[#030712] flex-[1_0_0] min-h-0 overflow-y-auto relative w-full [&::-webkit-scrollbar]:w-[6px] [&::-webkit-scrollbar-track]:bg-transparent [&::-webkit-scrollbar-thumb]:bg-[#374151] [&::-webkit-scrollbar-thumb]:rounded-full" data-name="Div">
-          <div className="flex flex-row justify-center size-full">
-            <div className="content-stretch flex items-start justify-center px-[32px] pt-[32px] pb-[80px] relative size-full">
-              <div className="content-stretch flex flex-[1_0_0] flex-col gap-[28px] h-full items-end max-w-[640px] min-w-px relative" data-name="FieldSet">
+          <div className="flex flex-row justify-center w-full">
+            <div className="content-stretch flex items-start justify-center px-[32px] pt-[32px] pb-[80px] relative w-full">
+              <div className="content-stretch flex flex-col gap-[28px] items-end max-w-[640px] min-w-px relative w-full" data-name="FieldSet">
                 <div className="content-stretch flex flex-col gap-[28px] items-start relative shrink-0 w-full" data-name="Field Group">
                   <div className="content-stretch flex flex-col items-start relative shrink-0 w-full" data-name="Div">
                     <div className="content-stretch flex items-start pb-[12px] relative shrink-0 w-full" data-name="Field / Legend">
